cb2ccc6584a45f2e9759cffca9a02b08827da157
Manav Yadav <manav.y1998@gmail.com>
2025-11-06 12:12:57 +0530

    Fixing merge related bugs

--- app/services/blueprints.js
+++ app/services/blueprints.js
@@ -17,12 +17,12 @@ export default class BlueprintsService extends Service {
   @tracked myBlueprints = [];
 
   @action
-  downloadCurrentBlueprint() {
+  async downloadCurrentBlueprint(j = '') {
     this.type.loadingSearchResults = true;
 
     if (this.isValidURL(j)) {
       j = await fetch(j);
-      j = await response.json(j);
+      j = await j.json();
     } else if (
       typeof j === 'object' &&
       j.isTrusted === undefined &&
@@ -149,42 +149,49 @@ export default class BlueprintsService extends Service {
 
         console.log(data_json);
 
-      if (type_slug == 'webapp') {
-        types_json['webapp'] = type_obj;
-      }
-    });
+        var types_json = [];
+        Object.entries(this.types.json.modules).forEach((v, i) => {
+          let type_slug = v[0];
+          let type_obj = v[1];
 
-    types_json['webapp']['implementation_summary'] = implementationSummary;
+          if (type_slug == 'webapp') {
+            types_json['webapp'] = type_obj;
+          }
+        });
 
-    if (data_json !== undefined && data_json) {
-      var link_json = [];
-      Object.entries(data_json).forEach((v, i) => {
-        let type_slug = v[0];
-        let type_obj = v[1];
+        types_json['webapp']['implementation_summary'] = implementationSummary;
 
-        if (type_slug != 'webapp') {
-          link_json[type_slug] = type_obj;
+        if (data_json !== undefined && data_json) {
+          var link_json = [];
+          Object.entries(data_json).forEach((v, i) => {
+            let type_slug = v[0];
+            let type_obj = v[1];
+
+            if (type_slug != 'webapp') {
+              link_json[type_slug] = type_obj;
+            }
+          });
+
+          this.types.json.modules = {
+            ...Object.assign({}, types_json),
+            ...Object.assign({}, link_json),
+          };
+          await this.types.json.save();
+
+          if (implementationSummary != '') {
+            later(
+              this,
+              () => {
+                window.location.href = '/#showImplementationSummary';
+                window.location.reload(true);
+              },
+              300,
+            );
+          } else window.location.href = '/';
+        } else {
+          this.type.loadingSearchResults = false;
         }
-      });
-
-      this.types.json.modules = {
-        ...Object.assign({}, types_json),
-        ...Object.assign({}, link_json),
-      };
-      await this.types.json.save();
-
-      if (implementationSummary != '') {
-        later(
-          this,
-          () => {
-            window.location.href = '/#showImplementationSummary';
-            window.location.reload(true);
-          },
-          300,
-        );
-      } else window.location.href = '/';
-    } else {
-      this.type.loadingSearchResults = false;
+      }
     }
   }
 
