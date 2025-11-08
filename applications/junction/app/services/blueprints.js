381095465b65453c91df8f75ca849a65f3e94d61
Manav Yadav <manav.y1998@gmail.com>
2025-11-08 19:19:36 +0530

    transffered API call from JE
    Moved API calls to agent-api.truearch.io for types generation and dummy data generation.

--- applications/junction/app/services/blueprints.js
+++ applications/junction/app/services/blueprints.js
@@ -4,6 +4,7 @@ import { action } from '@ember/object';
 import { later } from '@ember/runloop';
 import { service } from '@ember/service';
 import ENV from 'junction/config/environment';
+import { Modal } from 'bootstrap';
 
 export default class BlueprintsService extends Service {
   @service colormodes;
@@ -16,8 +17,22 @@ export default class BlueprintsService extends Service {
   @tracked myBlueprints = [];
 
   @action
-  downloadCurrentBlueprint() {
+  async downloadCurrentBlueprint(j = '') {
     this.type.loadingSearchResults = true;
+
+    if (this.isValidURL(j)) {
+      j = await fetch(j);
+      j = await j.json();
+    } else if (
+      typeof j === 'object' &&
+      j.isTrusted === undefined &&
+      j !== null &&
+      !Array.isArray(j)
+    ) {
+      j = j;
+    } else {
+      j = this.types.json.modules;
+    }
 
     var types_json = [];
     Object.entries(this.types.json.modules).forEach((v, i) => {
@@ -26,6 +41,7 @@ export default class BlueprintsService extends Service {
 
       if (
         type_slug != 'deleted_record' &&
+        type_slug != 'platform_record' &&
         type_slug != 'blueprint_record' &&
         type_slug != 'file_record' &&
         type_slug != 'apikey_record'
@@ -67,56 +83,115 @@ export default class BlueprintsService extends Service {
   }
 
   @action
-  async changeBlueprint(link, implementationSummary = '') {
-    await this.types.saveCurrentTypes(this.types.json.modules);
-
+  async downloadCurrentSimplifiedBlueprint(j = '') {
     this.type.loadingSearchResults = true;
 
-    let data_json = await fetch(link).then(function (response) {
-      return response.json();
-    });
+    let types_json = this.types.simplifiedJson;
 
-    var types_json = [];
-    Object.entries(this.types.json.modules).forEach((v, i) => {
-      let type_slug = v[0];
-      let type_obj = v[1];
-
-      if (type_slug == 'webapp') {
-        types_json['webapp'] = type_obj;
-      }
-    });
-
-    types_json['webapp']['implementation_summary'] = implementationSummary;
-
-    if (data_json !== undefined && data_json) {
-      var link_json = [];
-      Object.entries(data_json).forEach((v, i) => {
-        let type_slug = v[0];
-        let type_obj = v[1];
-
-        if (type_slug != 'webapp') {
-          link_json[type_slug] = type_obj;
-        }
-      });
-
-      this.types.json.modules = {
-        ...Object.assign({}, types_json),
-        ...Object.assign({}, link_json),
-      };
-      await this.types.json.save();
-
-      if (implementationSummary != '') {
-        later(
-          this,
-          () => {
-            window.location.href = '/#showImplementationSummary';
-            window.location.reload(true);
-          },
-          300,
+    later(
+      this,
+      () => {
+        const jsonString = JSON.stringify(
+          Object.fromEntries(Object.entries(types_json)),
+          null,
+          2,
         );
-      } else window.location.href = '/';
-    } else {
-      this.type.loadingSearchResults = false;
+
+        // Create a Blob from the JSON string
+        const blob = new Blob([jsonString], { type: 'application/json' });
+
+        // Create a link element
+        let t = Math.floor(new Date().getTime() / 1000);
+
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = 'Blueprint-' + t + '.simplified-types.json';
+
+        // Append to the document and trigger the download
+        document.body.appendChild(link);
+        link.click();
+
+        // Clean up and remove the link
+        document.body.removeChild(link);
+
+        this.type.loadingSearchResults = false;
+      },
+      1000,
+    );
+  }
+
+  @action
+  async changeBlueprint(j, implementationSummary = '') {
+    if (j.modules !== undefined) {
+      this.type.loadingSearchResults = true;
+
+      if (!this.isValidURL(j) && j.modules.types_json !== undefined) {
+        let t = j.modules.types_json;
+        this.types.json.modules = t;
+        await this.types.json.save();
+        window.location.href = '/';
+      } else if (j.modules.link !== undefined || j.modules.url !== undefined) {
+        let link =
+          j.modules.link === undefined ? j.modules.url : j.modules.link;
+        await this.types.saveCurrentTypes(this.types.json.modules);
+
+        // Instead of fetching the external URL directly
+        let response = await fetch(
+          'https://tribe.junction.express/custom/get-types.php',
+          {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ link: link }),
+          },
+        );
+
+        let data_json = await response.json();
+
+        console.log(data_json);
+
+        var types_json = [];
+        Object.entries(this.types.json.modules).forEach((v, i) => {
+          let type_slug = v[0];
+          let type_obj = v[1];
+
+          if (type_slug == 'webapp') {
+            types_json['webapp'] = type_obj;
+          }
+        });
+
+        types_json['webapp']['implementation_summary'] = implementationSummary;
+
+        if (data_json !== undefined && data_json) {
+          var link_json = [];
+          Object.entries(data_json).forEach((v, i) => {
+            let type_slug = v[0];
+            let type_obj = v[1];
+
+            if (type_slug != 'webapp') {
+              link_json[type_slug] = type_obj;
+            }
+          });
+
+          this.types.json.modules = {
+            ...Object.assign({}, types_json),
+            ...Object.assign({}, link_json),
+          };
+          await this.types.json.save();
+
+          if (implementationSummary != '') {
+            later(
+              this,
+              () => {
+                window.location.href = '/#showImplementationSummary';
+                window.location.reload(true);
+              },
+              300,
+            );
+          } else window.location.href = '/';
+        } else {
+          this.type.loadingSearchResults = false;
+        }
+      }
     }
   }
 
@@ -163,25 +238,35 @@ export default class BlueprintsService extends Service {
 
   @action
   async getBlueprints() {
+    this.myBlueprints = await this.store.query('blueprint_record', {
+      show_public_objects_only: false,
+    });
+
     if (
+      Object.entries(this.types.json.modules).length <= 6 &&
+      this.myBlueprints[0] !== undefined &&
+      this.myBlueprints[0].id !== undefined
+    ) {
+      let bp = new Modal(document.getElementById('blueprintAIPromptModal'), {});
+      bp.show();
+    } else if (
       this.auth.projectDescription != '' &&
-      Object.entries(this.types.json.modules).length <= 5
+      Object.entries(this.types.json.modules).length <= 6
     ) {
       await this.getAI();
     } else if (
       this.auth.blueprintLink != '' &&
-      Object.entries(this.types.json.modules).length <= 5
+      Object.entries(this.types.json.modules).length <= 6
     ) {
-      this.changeBlueprint(
+      await this.changeBlueprint(
         this.auth.blueprintLink,
         this.auth.implementationSummary,
       );
     } else {
-      let data = await fetch(
+      let response = await fetch(
         'https://tribe.junction.express/api.php/blueprint',
-      ).then(function (response) {
-        return response.json();
-      });
+      );
+      let data = await response.json();
       this.junctionBlueprints = data.data;
       this.myBlueprints = await this.store.query('deleted_record', {
         modules: { deleted_type: 'blueprint_record' },
@@ -205,13 +290,14 @@ export default class BlueprintsService extends Service {
   @tracked loadingProgress = 0;
   @tracked tryAgain = false;
   @tracked totalTime = 0;
+  @tracked intervalId = 0;
 
   progressLoading = () => {
-    if (this.loadingProgress < 89) this.loadingProgress += 10;
+    if (this.loadingProgress < 95) this.loadingProgress += 5;
     this.totalTime += 5;
 
-    if (this.totalTime > 70) {
-      clearInterval(intervalId);
+    if (this.totalTime > 120) {
+      clearInterval(this.intervalId);
       this.getAI();
     }
   };
@@ -223,10 +309,10 @@ export default class BlueprintsService extends Service {
       this.totalTime = 5;
       await this.types.saveCurrentTypes(this.types.json.modules);
 
-      let intervalId = setInterval(this.progressLoading, 5000);
+      this.intervalId = setInterval(this.progressLoading, 5000);
 
-      let data = await fetch(
-        'https://tribe.junction.express/custom/anthropic/get-response.php',
+      let response = await fetch(
+        'https://agent-api.truearch.io/api/v1/generate-types',
         {
           method: 'POST',
           headers: {
@@ -236,19 +322,18 @@ export default class BlueprintsService extends Service {
             project_description: this.projectDescription,
           }),
         },
-      ).then(function (response) {
-        return response.json();
-      });
+      );
+      let data = await response.json();
 
-      if (data !== undefined && data && data.json) {
+      if (data !== undefined && data && data.types) {
         if (
           (data.error !== undefined && data.error) ||
-          data.json === undefined
+          data.types === undefined
         ) {
           this.loadingProgress = 0;
           this.tryAgain = true;
         } else {
-          let data_json = data.json;
+          let data_json = data.types;
 
           if (data_json === undefined) {
             this.loadingProgress = 0;
@@ -276,7 +361,7 @@ export default class BlueprintsService extends Service {
 
             types_json['webapp']['project_description'] =
               this.projectDescription;
-            types_json['webapp']['implementation_summary'] = data.html;
+            types_json['webapp']['implementation_summary'] = data.html ? data.html : "No summary";
 
             if (data_json) {
               this.types.json.modules = {
@@ -286,7 +371,7 @@ export default class BlueprintsService extends Service {
               await this.types.json.save();
 
               this.loadingProgress = 100;
-              clearInterval(intervalId);
+              clearInterval(this.intervalId);
             }
 
             later(
@@ -321,14 +406,15 @@ export default class BlueprintsService extends Service {
           type_slug != 'deleted_record' &&
           type_slug != 'file_record' &&
           type_slug != 'apikey_record' &&
+          type_slug != 'platform_record' &&
           type_slug != 'blueprint_record'
         ) {
           types_json[type_slug] = type_obj;
         }
       });
 
-      let data = await fetch(
-        'https://tribe.junction.express/custom/anthropic/get-sample-data.php',
+      let response = await fetch(
+        'https://agent-api.truearch.io/api/v1/generate-sample-data',
         {
           method: 'POST',
           headers: {
@@ -342,9 +428,8 @@ export default class BlueprintsService extends Service {
             types_json: { ...Object.assign({}, types_json) },
           }),
         },
-      ).then(function (response) {
-        return response.json();
-      });
+      );
+      let data = await response.json();
 
       if (data !== undefined && data && data.objects) {
         Object.entries(data.objects).forEach(async (v, i) => {
